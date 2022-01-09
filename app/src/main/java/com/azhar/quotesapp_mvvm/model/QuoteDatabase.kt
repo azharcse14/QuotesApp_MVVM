c2b23215f0e2e029e6300fa7755b8672a3a99d00
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2022-01-09 11:06:05 +0600

    add database

--- app/src/main/java/com/azhar/quotesapp_mvvm/model/QuoteDatabase.kt
+++ app/src/main/java/com/azhar/quotesapp_mvvm/model/QuoteDatabase.kt
@@ -1,4 +1,33 @@
 package com.azhar.quotesapp_mvvm.model
 
-class QuoteDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities = [Quote::class], version = 1)
+abstract class QuoteDatabase : RoomDatabase() {
+    abstract fun quoteDao(): QuoteDao
+
+    companion object {
+        private var INSTANCE: QuoteDatabase? = null
+        @InternalCoroutinesApi
+        fun getDatabase(context: Context): QuoteDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(
+                        context,
+                        QuoteDatabase::class.java,
+                        "quote_database"
+                    )
+                        .createFromAsset("quotes.db")
+                        .build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
 }
